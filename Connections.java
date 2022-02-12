26102e66af0ec5056567dd96213bb11b1076cdcd
Satya9154 <99533578+Satya9154@users.noreply.github.com>
2022-02-12 15:19:01 +0530

    Add files via upload

---
+++ Connections.java
@@ -0,0 +1,17 @@
+/*Connect
+The connect function is used to connect the database to the GUI. So, to do that, I have mentioned the below code:
+*/
+public static Connection connect()
+{
+try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //System.out.println("Loaded driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/mysql?user=root&password=edureka");
+        //System.out.println("Connected to MySQL");
+        return con;
+ } 
+ catch (Exception ex) {
+        ex.printStackTrace();
+ }
+return null;
+}
